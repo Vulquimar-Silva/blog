79c15a0ab72aefd2a8b9a55c7b76c708958a8592
Vulquimar-Silva <vuquimar@hotmail.com>
2022-10-19 03:40:41 -0300

    updating structure SASS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import logoSvg from '../../assets/img/svg/logo.svg'
 
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <header className='flex-space-between'>
+        <div className='logo'>
+          <img src={logoSvg} alt="Logo do Blog" />
+        </div>
+        <div className='search'>
+          <input type="text" name='search' className='input-search' placeholder='Buscar artigos, podcasts, destaques...' />
+        </div>
+        <ul className='menu'>
+          <li><a href="#" className='nav-link'>Categories</a></li>
+          <li><a href="#" className='nav-link'>About</a></li>
+          <li><a href="#" className='nav-link'>Contact</a></li>
+        </ul>
+      </header>
+    </>
   )
 }
